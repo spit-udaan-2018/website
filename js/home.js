91575299736a2f109ac98701175568188c5a45d6
Kaustubh Toraskar <kaustubhtoraskar97@gmail.com>
2018-02-06 01:31:31 +0530

    final

--- js/home.js
+++ js/home.js
@@ -12,6 +12,18 @@ $('.fancy-button').click(function(e) {
         sessionStorage.setItem(OPEN_IMAGE, target);
 })
 
+$('.dev-image').click(function(e) {
+    e.preventDefault();
+    var type = $(this).data('type');
+    
+    // for team images
+        var target = $(this).data('target');
+        $('#black-overlay').fadeIn();
+        $('#' + target + '-img').fadeIn();
+        console.log(target);
+        sessionStorage.setItem(OPEN_IMAGE, target);
+})
+
 $('.event-poster').click(function(e){
     var target = $(this).data('target');
     window.open('event/' + target);
